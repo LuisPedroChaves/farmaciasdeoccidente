c061251288980e98facda91a4cf093871d5fabea
luispedrochaves@hotmail.com <luispedrochaves@hotmail.com>
2020-12-07 12:44:52 -0600

    cambios

--- src/app/pages/modules/customers/components/customers/customers.component.ts
+++ src/app/pages/modules/customers/components/customers/customers.component.ts
@@ -20,20 +20,20 @@ export class CustomersComponent implements OnInit {
   configSubscription: Subscription;
   sessionsubscription: Subscription;
   // permissions
-  customersp: string[];
   smallScreen = window.innerWidth < 960 ? true : false;
 
   // costumers -------------
   selectedCostumer: any;
   costumersSubscription: Subscription;
   customers: any[] = [
-    { nit: '730613-k', name: 'Luis', address: '|Ciudad'},
+    // tslint:disable-next-line: max-line-length
+    { nit: '730613-k', name: 'Luis', address: '|Ciudad', phone: '2535345354', area: '3', town: 'almolonga', department: 'quetzaltenago', company: 'jjhkjh', transport: 'ngkugj', limitCredit:'100', limitDaysCredits:'8'},
     { nit: '730613-k', name: 'Luis', address: '|Ciudad'},
     { nit: '730613-k', name: 'Luis', address: '|Ciudad'},
     { nit: '730613-k', name: 'Luis', address: '|Ciudad'},
   ];
   dataSource = new MatTableDataSource();
-  columnsToDisplay = ['name', 'nit', 'address'];
+  columnsToDisplay = ['name', 'nit', 'address', 'phone'];
   columnsToDisplay2 = ['image', 'name', 'nit', 'address'];
   expandedElement: any | null;
   // HOOK FUNCTIONS //////////////////////////////////////////////////////////////////////////////////////////
@@ -43,6 +43,7 @@ export class CustomersComponent implements OnInit {
   }
 
   ngOnInit(): void {
+      this.dataSource = new MatTableDataSource(this.customers);
   }
 
 
@@ -52,7 +53,7 @@ export class CustomersComponent implements OnInit {
   newCostumer() {
     const dialogRef = this.dialog.open(NewCustomersComponent, {
       width: this.smallScreen ? '100%' : '500px',
-      data: { title: 'Nuevo Cliente', customersp: this.customersp },
+      data: { title: 'Nuevo Cliente'},
       disableClose: true,
       panelClass: ['iea-dialog' ],
     });
@@ -66,13 +67,12 @@ export class CustomersComponent implements OnInit {
   editCostumer(costumer: any) {
     const dialogRef = this.dialog.open(EditCustomerComponent, {
       width: this.smallScreen ? '100%' : '500px',
-      data: { title: 'Nuevo Cliente', client: costumer, customersp: this.customersp },
+      data: { title: 'Nuevo Cliente', client: costumer},
       disableClose: true,
       panelClass: ['iea-dialog' ],
     });
 
     dialogRef.afterClosed().subscribe(result => {
-   
     });
   }
 
