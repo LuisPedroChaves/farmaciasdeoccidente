668fe3ac59ddc44c9a987ea00a3519b43fdb15af
luispedrochaves@hotmail.com <luispedrochaves@hotmail.com>
2020-12-11 12:36:21 -0600

    tabla

--- src/app/pages/modules/customers/components/customers/customers.component.ts
+++ src/app/pages/modules/customers/components/customers/customers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, NgZone, AfterContentInit, OnDestroy } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Subscription } from 'rxjs';
 import { EventBusService } from '../../../../../core/services/internal/event-bus.service';
 import { ConfigService } from '../../../../../core/services/config/config.service';
--- src/app/pages/modules/receivables/components/receivables/receivables.component.ts
+++ src/app/pages/modules/receivables/components/receivables/receivables.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatTableDataSource } from '@angular/material/table';
+import { Subscription } from 'rxjs';
+import { ConfigService } from 'src/app/core/services/config/config.service';
+import { EventBusService } from 'src/app/core/services/internal/event-bus.service';
 
 @Component({
-  selector: 'app-receivables',
+  selector: 'app-banks',
   templateUrl: './receivables.component.html',
   styleUrls: ['./receivables.component.scss']
 })
-export class ReceivablesComponent implements OnInit {
+export class ReceivablesComponent implements OnInit, AfterContentInit, OnDestroy {
+  // screen--------------------------------------------------------------------------------------------
+  // subscriptions ------------------
+  configSubscription: Subscription;
+  sessionsubscription: Subscription;
+  accountssubscription: Subscription;
+  bankssubscription: Subscription;
+  @ViewChild('bankbar') bankbar: MatSidenav;
+  smallScreen = window.innerWidth < 960 ? true : false;
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+  // accounts
+  receivables: any[];
+  banks: any[];
+  dataSource = new MatTableDataSource<any>();
+  displayedColumns: string[] = ['customer', 'employee', 'name', 'no.invoice','date' ,'balance' ,'state' , 'credit','creditDays'];
+  title = 'Bancos';
+  selectedAccount: any;
+  constructor(
+              public eventBus: EventBusService, public config: ConfigService) { }
+
+  ngOnInit(): void { this.dataSource = new MatTableDataSource(this.receivables);
+
   }
+
+
+  ngAfterContentInit() {
+
+  }
+
+  ngOnDestroy() {
+    this.configSubscription?.unsubscribe();
+    this.sessionsubscription?.unsubscribe();
+    this.bankssubscription?.unsubscribe();
+    this.accountssubscription?.unsubscribe();
+  }
+
+  applyFilter2(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 
 }
