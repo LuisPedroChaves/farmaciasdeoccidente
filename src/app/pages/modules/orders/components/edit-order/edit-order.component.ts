f8d9f1a009dee561930366eb16603cf3ceb9a05a
luispedrochaves@hotmail.com <luispedrochaves@hotmail.com>
2021-01-08 18:55:31 -0600

    cambios

--- src/app/pages/modules/orders/components/edit-order/edit-order.component.ts
+++ src/app/pages/modules/orders/components/edit-order/edit-order.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-edit-order',
   templateUrl: './edit-order.component.html',
   styleUrls: ['./edit-order.component.scss']
 })
 export class EditOrderComponent implements OnInit {
-
-  constructor() { }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  editMode = false;
+  form = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    nit: new FormControl(null,  [Validators.required]),
+    address: new FormControl(null),
+  constructor(public dialogRef: MatDialogRef<EditOrderRoutesComponent>, @Inject(MAT_DIALOG_DATA) public data: any, public dialog: MatDialog) { }
 
   ngOnInit(): void {
+    console.log(this.data);
+    this.form = new FormGroup({
+      name: new FormControl(this.data.client.name, [Validators.required]),
+      nit: new FormControl(this.data.client.nit,  [Validators.required]),
+      address: new FormControl(this.data.client.address),
+      phone: new FormControl(this.data.client.phone),
+      area: new FormControl(this.data.client.area),
+      town: new FormControl(this.data.client.town),
+      department: new FormControl(this.data.client.department),
+      company: new FormControl(this.data.client.company),
+      transport: new FormControl(this.data.client.transport),
+      limitCredit: new FormControl(this.data.client.limitCredit),
+      limitDaysCredits: new FormControl(this.data.client.limitDaysCredits),
+    });
+  }
+}
+
   }
 
 }
--- src/app/pages/modules/orders/components/new-order/new-order.component.ts
+++ src/app/pages/modules/orders/components/new-order/new-order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-order',
@@ -6,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-order.component.scss']
 })
 export class NewOrderComponent implements OnInit {
-
-  constructor() { }
+  form = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    nit: new FormControl(null,  [Validators.required]),
+    address: new FormControl(null, ),
+  });
+  constructor(public dialogRef: MatDialogRef<NewOrderRoutesComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
   }
