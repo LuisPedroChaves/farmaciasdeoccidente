c7a70e25fcdd2b981c165198ea07bc494ffae8ea
Lotus <egoo.geovany@gmail.com>
2021-11-30 11:31:53 -0600

    Table of statistics

--- src/app/pages/admin-modules/uploads/components/statistics/statistics.component.ts
+++ src/app/pages/admin-modules/uploads/components/statistics/statistics.component.ts
@@ -1,5 +1,14 @@
-import { AfterContentInit, Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Observable, Subscription } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { BrandItem } from 'src/app/core/models/Brand';
@@ -45,13 +54,30 @@ export class StatisticsComponent
   loadingData = false;
   setData = false;
 
+  // Table
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  dataSource: any;
+  columns = [
+    'avgSalesMonths',
+    'avgSalesYear',
+    'salesLastMonth',
+    'avgSalesDay',
+    'inventory',
+    'sales',
+    'suggestedOrder',
+    'minExistence',
+    'maxExistence',
+  ];
+  currentPage = 0;
+  // End Table
+
   constructor(
     public cellarService: CellarService,
     public tempStorageService: TempStorageService,
     public tempSaleService: TempSaleService,
     public brandService: BrandService,
-    private toastyService: ToastyService,
-
+    private toastyService: ToastyService
   ) {
     this.cellarsSubscription = this.cellarService
       .readData()
@@ -80,6 +106,7 @@ export class StatisticsComponent
     this.cellarsSubscription.unsubscribe();
   }
 
+
   private _filterBrands(value: string): BrandItem[] {
     if (value) {
       const filterValue = value.toLowerCase();
@@ -96,35 +123,45 @@ export class StatisticsComponent
       this.toastyService.error('Debe seleccionar una sucursal');
       return;
     }
+    if ( this.daysOfRequest === '' || this.supplyDays === '') {
+      this.toastyService.error('Llene los campos solicitados');
+      return;
+    }
     const brand = this.brands.find(
       (e) => e.name === this.range.controls._brand.value
     );
     if (brand) {
       this.loadingData = true;
       this.setData = true;
-      console.log(        brand._id,
+      console.log(
+        brand._id,
         this.getDate(this.range.controls.startDate.value),
         this.getDate(this.range.get('endDate').value),
         this.daysOfRequest,
-        this.supplyDays);
-      this.tempSaleService.getStatics( this.currentCellar2,
-        brand._id,
-        this.range.controls.startDate.value,
-        this.range.get('endDate').value,
-        this.daysOfRequest,
-        this.supplyDays).subscribe((res) => {
-          console.log(res);
+        this.supplyDays
+      );
+      this.tempSaleService
+        .getStatics(
+          this.currentCellar2,
+          brand._id,
+          this.range.controls.startDate.value,
+          this.range.get('endDate').value,
+          this.daysOfRequest,
+          this.supplyDays
+        )
+        .subscribe((res) => {
+          const response = res;
+          this.dataSource = new MatTableDataSource<any>(response.tempSales);
+          this.dataSource.paginator = this.paginator;
           this.loadingData = false;
         });
-
     } else {
       this.toastyService.error('Seleccione un laboratorio válido');
     }
-
   }
 
   getDate(date: any): string {
     return date._d;
   }
-
 }
+
