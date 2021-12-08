698b95556c12a3cf8dbf13bb79970d98d5c69eec
LuisPedroChaves <luispedrochaves@hotmail.com>
2021-12-08 13:21:58 -0600

    Confilcto en master

--- src/app/core/models/TempStorage.ts
+++ src/app/core/models/TempStorage.ts
@@ -5,7 +5,7 @@ export interface TempStorageItem {
     _cellar: CellarItem;
     _product: ProductItem;
     stock: number,
+    supply: number,
     minStock: number,
     maxStock: number,
-    supply: number,
 }
