4758175f720c5a2fdfb156cccc10c1bd5d36d3eb
LuisPedroChaves <luispedrochaves@hotmail.com>
2022-05-02 22:07:52 -0600

    Avances de ventas

--- src/app/pages/modules/sales/components/new-sale/new-sale.component.ts
+++ src/app/pages/modules/sales/components/new-sale/new-sale.component.ts
@@ -1,21 +1,29 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { AfterContentInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 import { Observable, Subscription } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
+import { CellarItem } from 'src/app/core/models/Cellar';
+
 import { CustomerItem } from 'src/app/core/models/Customer';
+import { UserItem } from 'src/app/core/models/User';
 import { CustomerService } from 'src/app/core/services/httpServices/customer.service';
+import { SaleService } from 'src/app/core/services/httpServices/sale.service';
+import { UserService } from 'src/app/core/services/httpServices/user.service';
 import { ToastyService } from 'src/app/core/services/internal/toasty.service';
-import { UserItem } from '../../../../../core/models/User';
-import { SaleService } from '../../../../../core/services/httpServices/sale.service';
-import { UserService } from '../../../../../core/services/httpServices/user.service';
 
 @Component({
   selector: 'app-new-sale',
   templateUrl: './new-sale.component.html',
   styleUrls: ['./new-sale.component.scss']
 })
-export class NewSaleComponent implements OnInit {
+export class NewSaleComponent implements OnInit, AfterContentInit, OnDestroy {
+
+  @Input()
+  currentCellar: CellarItem;
+
+  @Output()
+  close = new EventEmitter();
 
   loading = false;
 
@@ -37,53 +45,92 @@ export class NewSaleComponent implements OnInit {
     noBill: new FormControl(null, [Validators.required]),
     total: new FormControl('', [Validators.required]),
   });
-
   idCustomer = null;
   timeAvaliable = true;
   credit = 0;
 
-  usersSubscription: Subscription;
+  customerSubscription: Subscription;
+  customers: CustomerItem[];
+  filteredOptions: Observable<CustomerItem[]>;
+  _customer = new FormControl(null, Validators.required);
+
+  userSubscription: Subscription;
   sellers: UserItem[];
 
-  // Autocompletado
-  orderFind = false;
-  customersSubscription: Subscription;
-  customers: CustomerItem[];
-  options: CustomerItem[] = [];
-  filteredOptions: Observable<CustomerItem[]>;
-
   constructor(
-    public dialogRef: MatDialogRef<NewSaleComponent>, @Inject(MAT_DIALOG_DATA) public data: any,
-    public toasty: ToastyService,
-    public customerService: CustomerService,
-    public saleService: SaleService,
-    public userService: UserService
-  ) { }
+    private customerService: CustomerService,
+    private userService: UserService,
+    private toastyService: ToastyService,
+    private saleService: SaleService
+  ) {
+    this.customerSubscription = this.customerService.readData().subscribe((data) => {
+      this.customers = data.filter(customer => customer.code);
+    });
+  }
 
   ngOnInit(): void {
-    this.customersSubscription = this.customerService.readData().subscribe(data => {
-      this.customers = data.filter(customer => customer.code);
-      this.options = [...this.customers];
-    });
+    this.filteredOptions = this._customer.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filterCustomers(value))
+    );
 
-    this.filteredOptions = this.form.controls.code.valueChanges.pipe(startWith(''), map(value => this._filter(value)));
-
-    this.usersSubscription = this.userService.readData().subscribe(data => {
+    this.userSubscription = this.userService.readData().subscribe(data => {
       this.sellers = data.filter(user => user._role.type === 'SELLER');
     });
   }
 
-  ngAfterContentInit() {
+  ngAfterContentInit(): void {
     this.customerService.loadData();
     this.userService.loadData();
   }
 
-  ngOnDestroy() {
-    this.customersSubscription?.unsubscribe();
-    this.usersSubscription?.unsubscribe();
+  ngOnDestroy(): void {
+    this.customerSubscription?.unsubscribe();
+    this.userSubscription?.unsubscribe();
   }
 
-  clear() {
+  private _filterCustomers(value: string): CustomerItem[] {
+    if (value) {
+      const filterValue = value.toLowerCase();
+      return this.customers.filter((option) =>
+        option.code.toLowerCase().includes(filterValue) || option.name.toLowerCase().includes(filterValue)
+      );
+    } else {
+      return [];
+    }
+  }
+
+  selected(code: string) {
+    const INDEX = this.customers.findIndex(c => c.code === code);
+    if (INDEX > -1) {
+      this.loading = true;
+      this.form.controls.code.setValue(this.customers[INDEX].code);
+      this.form.controls.nit.setValue(this.customers[INDEX].nit);
+      this.form.controls.name.setValue(this.customers[INDEX].name);
+      this.form.controls.phone.setValue(this.customers[INDEX].phone);
+      this.form.controls.address.setValue(this.customers[INDEX].address);
+      this.form.controls.town.setValue(this.customers[INDEX].town);
+      this.form.controls.department.setValue(this.customers[INDEX].department);
+      this.form.controls.company.setValue(this.customers[INDEX].company);
+      this.form.controls.transport.setValue(this.customers[INDEX].transport);
+      this.form.controls.limitCredit.setValue(this.customers[INDEX].limitCredit);
+      this.form.controls.limitDaysCredit.setValue(this.customers[INDEX].limitDaysCredit);
+      this.form.controls._seller.setValue(this.customers[INDEX]._seller._id);
+      this.idCustomer = this.customers[INDEX]._id;
+      this.customerService.getCustomer(this.idCustomer).subscribe(data => {
+        this.timeAvaliable = data.timeAvaliable;
+        this.credit = data.credit;
+        this.loading = false;
+      });
+    }
+    else {
+      this.resetForm();
+    }
+  }
+
+  resetForm(): void {
+    this._customer.setValue('')
+
     this.form.controls.nit.setValue('');
     this.form.controls.name.setValue('');
     this.form.controls.phone.setValue('');
@@ -105,78 +152,27 @@ export class NewSaleComponent implements OnInit {
     this.credit = 0;
   }
 
-  // Autocompletado
-  findThis(value) {
-    if (value !== 'cf') {
-      this.orderFind = false;
-      const index = this.customers.findIndex(c => c.code === value);
-      if (index > -1) {
-        this.loading = true;
-        this.orderFind = true;
-        this.form.controls.nit.setValue(this.customers[index].nit);
-        this.form.controls.name.setValue(this.customers[index].name);
-        this.form.controls.phone.setValue(this.customers[index].phone);
-        this.form.controls.address.setValue(this.customers[index].address);
-        this.form.controls.town.setValue(this.customers[index].town);
-        this.form.controls.department.setValue(this.customers[index].department);
-        this.form.controls.company.setValue(this.customers[index].company);
-        this.form.controls.transport.setValue(this.customers[index].transport);
-        this.form.controls.limitCredit.setValue(this.customers[index].limitCredit);
-        this.form.controls.limitDaysCredit.setValue(this.customers[index].limitDaysCredit);
-        this.form.controls._seller.setValue(this.customers[index]._seller._id);
-        this.idCustomer = this.customers[index]._id;
-        this.customerService.getCustomer(this.idCustomer).subscribe(data => {
-          this.timeAvaliable = data.timeAvaliable;
-          this.credit = data.credit;
-          this.loading = false;
-        });
-      }
-    } else {
-      this.form.controls.nit.setValue('');
-      this.form.controls.name.setValue('');
-      this.form.controls.phone.setValue('');
-      this.form.controls.address.setValue('');
-      this.form.controls.town.setValue('');
-      this.form.controls.department.setValue('Huehuetenango');
-      this.form.controls.company.setValue('');
-      this.form.controls.transport.setValue('');
-      this.form.controls.limitCredit.setValue('');
-      this.form.controls.limitDaysCredit.setValue('');
-      this.form.controls._seller.setValue('');
-    }
-  }
-
-  private _filter(value: string): CustomerItem[] {
-    if (value) {
-      const filterValue = value.toLowerCase();
-      return this.options.filter(option => option.code.toLowerCase().includes(filterValue));
-    } else {
-      return [];
-    }
-  }
-
-  saveSale() {
+  save(): void {
     if (this.form.invalid) { return; }
     this.credit = parseFloat(this.credit.toFixed(2));
     if (parseFloat(this.form.get('total').value) > this.credit) {
-      this.toasty.error('No hay suficiente crédito disponible');
+      this.toastyService.error('No hay suficiente crédito disponible');
       return;
     }
     this.loading = true;
-    this.form.get('_cellar').setValue(this.data.currentCellar);
+    this.form.get('_cellar').setValue(this.currentCellar);
     const sale: any = { ...this.form.value };
     this.saleService.createSale(sale).subscribe(data => {
       if (data.ok === true) {
-        this.toasty.success('Venta creada exitosamente');
-        this.dialogRef.close('ok');
+        this.toastyService.success('Venta creada exitosamente');
         this.loading = false;
       } else {
         this.loading = false;
-        this.toasty.error('Error al crear la venta');
+        this.toastyService.error('Error al crear la venta');
       }
     }, err => {
       this.loading = false;
-      this.toasty.error('Error al crear la venta');
+      this.toastyService.error('Error al crear la venta');
     });
   }
 
